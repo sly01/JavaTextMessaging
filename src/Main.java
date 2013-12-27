bc47186ab04cc9e3d530994459b62201401d98a9
erkoc <aerkoc01@gmail.com>
2013-12-27 17:47:23 +0200

    Program updated

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -23,29 +24,42 @@ import javax.swing.WindowConstants;
  * @author erkoc
  */
 public class Main extends JFrame {
-    
+
     public static void main(String[] args) {
-        
+
         final StringBuffer test = new StringBuffer();
         JFrame frame = new JFrame();
         frame.setTitle("Text Message Application");
         frame.setSize(600, 600);
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.setLayout(new GridLayout(3, 1));
-        
+
+        JLabel lbl1 = new JLabel();
+        JLabel lbl2 = new JLabel();
+        JLabel lbl3 = new JLabel();
+        JLabel lbl4 = new JLabel();
+        JLabel lbl5 = new JLabel();
+        JLabel lbl6 = new JLabel();
         final JTextField txtField = new JTextField(30);
         final JTextArea txtArea = new JTextArea();
         JButton send = new JButton("Send");
-        
+
         JPanel panel1 = new JPanel();
         JPanel panel2 = new JPanel();
         JPanel panel3 = new JPanel();
         panel1.setLayout(new BorderLayout());
+        panel2.setLayout(new GridLayout(4, 2));
         panel1.add(txtArea);
+        panel2.add(lbl1);
+        panel2.add(lbl2);
+        panel2.add(lbl3);
+        panel2.add(lbl4);
+        panel2.add(lbl5);
+        panel2.add(lbl6);
         panel2.add(txtField);
         panel2.add(send);
         panel3.setLayout(new GridLayout(4, 1));
-        
+
         String row1 = "1234567890";
         String row2 = "qwertyuiop";
         String row3 = "asdfghjkl;";
@@ -66,7 +80,7 @@ public class Main extends JFrame {
                 });
             }
         }
-        
+
         send.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
@@ -74,17 +88,17 @@ public class Main extends JFrame {
                 txtField.setText(null);
             }
         });
-        
+
         panel1.setBackground(Color.red);
         panel2.setBackground(Color.black);
         panel3.setBackground(Color.white);
-        
-        
+
+
         Container container = frame.getContentPane();
         container.add(panel1, BorderLayout.NORTH);
         container.add(panel2, BorderLayout.CENTER);
         container.add(panel3, BorderLayout.SOUTH);
-        
+
         frame.setVisible(true);
     }
 }
